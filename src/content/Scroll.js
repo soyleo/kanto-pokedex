3d7fb579d746025ad30c386944439e083dfe2e04
soyleo <leopoldo230791@gmail.com>
2021-02-28 22:17:58 -0300

    some fixes

--- src/content/Scroll.js
+++ src/content/Scroll.js
@@ -1,6 +1,6 @@
 const Scroll = (props) => {
     return (
-        <div style={{ overflowY: 'scroll', height: '75vh' }}>
+        <div style={{ overflowY: 'scroll', height: '60vh' }}>
             {props.children}
         </div>
     );
